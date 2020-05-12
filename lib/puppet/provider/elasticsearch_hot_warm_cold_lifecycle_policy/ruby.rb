e6165ebb66947632e6b788f618a008545c59e112
Gustavo Yoshizaki <gustavo.yoshizaki@avature.net>
2020-05-12 18:02:12 -0300

    Test, only keep 2 properties.

--- lib/puppet/provider/elasticsearch_hot_warm_cold_lifecycle_policy/ruby.rb
+++ lib/puppet/provider/elasticsearch_hot_warm_cold_lifecycle_policy/ruby.rb
@@ -11,39 +11,13 @@ Puppet::Type.type(:elasticsearch_hot_warm_cold_lifecycle_policy).provide(
   mk_resource_methods
   def self.process_body(body)
     phases = JSON.parse(body)['policy']['phases']
-    hotPhasePresent = phases['hot'] != nil
-    warmPhasePresent = phases['warm'] != nil
     coldPhasePresent = phases['cold'] != nil
-    deletePhasePresent = phases['delete'] != nil
     results =
       {
         :name                              => 'policy',
         :ensure                            => :present,
-        :hot_priority                      => if !hotPhasePresent then nil else phases['hot']['priority'] end,
-        :hot_rollover_max_size             => if !hotPhasePresent || phases['hot']['actions']['rollover'].nil? then nil else phases['hot']['actions']['rollover']['max_size'] end,
-        :hot_rollover_max_docs             => if !hotPhasePresent || phases['hot']['actions']['rollover'].nil? then nil else phases['hot']['actions']['rollover']['max_docs'] end,
-        :hot_rollover_max_age              => if !hotPhasePresent || phases['hot']['actions']['rollover'].nil? then nil else phases['hot']['actions']['rollover']['max_age'] end,
-        :hot_unfollow                      => if !hotPhasePresent then nil else phases['hot']['actions']['unfollow'] end,
-        :warm_min_age                      => if !warmPhasePresent then nil else phases['warm']['min_age'] end,
-        :warm_priority                     => if !warmPhasePresent then nil else phases['warm']['priority'] end,
-        :warm_unfollow                     => if !warmPhasePresent then nil else phases['warm']['actions']['unfollow'] end,
-        :warm_allocate_number_of_replicas  => if !warmPhasePresent || phases['warm']['actions']['allocate'].nil? then nil else phases['warm']['actions']['allocate']['number_of_replicas'] end,
-        :warm_allocate_include             => if !warmPhasePresent || phases['warm']['actions']['allocate'].nil? then nil else phases['warm']['actions']['allocate']['include'] end,
-        :warm_allocate_exclude             => if !warmPhasePresent || phases['warm']['actions']['allocate'].nil? then nil else phases['warm']['actions']['allocate']['exclude'] end,
-        :warm_allocate_require             => if !warmPhasePresent || phases['warm']['actions']['allocate'].nil? then nil else phases['warm']['actions']['allocate']['require'] end,
-        :warm_read_only                    => if !warmPhasePresent then nil else phases['warm']['actions']['read_only'] end,
-        :warm_force_merge_max_num_segments => if !warmPhasePresent || phases['warm']['actions']['forcemerge'].nil? then nil else phases['warm']['actions']['forcemerge']['max_num_segments'] end,
-        :warm_shrink_number_of_shards      => if !warmPhasePresent || phases['warm']['actions']['shrink'].nil? then nil else phases['warm']['actions']['shrink']['number_of_shards'] end,
         :cold_min_age                      => if !coldPhasePresent then nil else phases['cold']['min_age'] end,
-        :cold_priority                     => if !coldPhasePresent then nil else phases['cold']['priority'] end,
-        :cold_allocate_number_of_replicas  => if !coldPhasePresent || phases['cold']['actions']['allocate'].nil? then nil else phases['cold']['actions']['allocate']['number_of_replicas'] end,
-        :cold_allocate_include             => if !coldPhasePresent || phases['cold']['actions']['allocate'].nil? then nil else phases['cold']['actions']['allocate']['include'] end,
-        :cold_allocate_exclude             => if !coldPhasePresent || phases['cold']['actions']['allocate'].nil? then nil else phases['cold']['actions']['allocate']['exclude'] end,
         :cold_allocate_require             => if !coldPhasePresent || phases['cold']['actions']['allocate'].nil? then nil else phases['cold']['actions']['allocate']['require'] end,
-        :cold_freeze                       => if !coldPhasePresent then nil else phases['cold']['actions']['freeze'] end,
-        :cold_unfollow                     => if !coldPhasePresent then nil else phases['cold']['actions']['unfollow'] end,
-        :delete_min_age                    => if !deletePhasePresent then nil else phases['delete']['min_age'] end,
-        :delete_delete                     => if !deletePhasePresent then nil else phases['delete']['actions']['delete'] end,
         :provider                          => name
       }
     results
@@ -52,69 +26,20 @@ Puppet::Type.type(:elasticsearch_hot_warm_cold_lifecycle_policy).provide(
   def generate_body
     body = {
       'phases' => {
-        'hot'    => {
-          'actions' => {
-            'set_priority' => {
-              'priority' => resource[:hot_priority]
-            }
-          }
-        },
-        'warm'   => {
-          'actions' => {
-            'set_priority' => {
-              'priority' => resource[:warm_priority]
-            }
-          }
-        },
         'cold'   => {
           'actions' => {
-            'set_priority' => {
-              'priority' => resource[:cold_priority]
-            }
           }
-        },
-        'delete' => {
-          'actions' => {}
         }
       }
     }
     setOrDelete(
-      body['phases']['hot']['actions'],
-      resource[:hot_rollover_max_size].nil? || !resource[:hot_rollover_max_docs].nil? || !resource[:hot_rollover_max_age].nil?,
-      'rollover',
-      {}
-    )
-    setOrDelete(body['phases']['hot']['actions']['rollover'], !resource[:hot_rollover_max_size].nil?, 'max_size', resource[:hot_rollover_max_size])
-    setOrDelete(body['phases']['hot']['actions']['rollover'], !resource[:hot_rollover_max_docs].nil?, 'max_docs', resource[:hot_rollover_max_docs])
-    setOrDelete(body['phases']['hot']['actions']['rollover'], !resource[:hot_rollover_max_age].nil?, 'max_age', resource[:hot_rollover_max_age])
-    setOrDelete(body['phases']['hot']['actions'], resource[:hot_unfollow], 'unfollow', {})
-    setOrDelete(
-      body['phases']['warm']['actions'],
-      !resource[:warm_allocate_number_of_replicas].nil? || !resource[:warm_allocate_include].ni? || !resource[:warm_allocate_exclude].nil? || !resource[:warm_allocate_require].nil?,
-      'allocate',
-      {}
-    )
-    setOrDelete(body['phases']['warm']['actions']['allocate'], !resource[:warm_allocate_number_of_replicas].nil?, 'number_of_replicas', resource[:warm_allocate_number_of_replicas])
-    setOrDelete(body['phases']['warm']['actions']['allocate'], !resource[:warm_allocate_include].nil?, 'include', resource[:warm_allocate_include])
-    setOrDelete(body['phases']['warm']['actions']['allocate'], !resource[:warm_allocate_exclude].nil?, 'exclude', resource[:warm_allocate_exclude])
-    setOrDelete(body['phases']['warm']['actions']['allocate'], !resource[:warm_allocate_require].nil?, 'require', resource[:warm_allocate_require])
-    setOrDelete(body['phases']['warm']['actions'], !resource[:warm_read_only], 'readonly', {})
-    setOrDelete(body['phases']['warm']['actions'], !resource[:warm_force_merge_max_num_segments], 'forcemerge', {'max_num_segments' => resource[:warm_force_merge_max_num_segments]})
-    setOrDelete(body['phases']['warm']['actions'], !resource[:warm_shrink_number_of_shards], 'shrink', {'number_of_shards' => resource[:warm_shrink_number_of_shards]})
-    setOrDelete(body['phases']['warm']['actions'], !resource[:warm_unfollow], 'unfollow', {})
-    setOrDelete(
       body['phases']['cold']['actions'],
-      !resource[:cold_allocate_number_of_replicas].nil? || !resource[:cold_allocate_include].nil? || !resource[:cold_allocate_exclude].nil? || !resource[:cold_allocate_require].nil?,
+      !resource[:cold_allocate_require].nil?,
       'allocate',
       {}
     )
-    setOrDelete(body['phases']['cold']['actions']['allocate'], !resource[:cold_allocate_number_of_replicas].nil?, 'number_of_replicas', resource[:cold_allocate_number_of_replicas])
-    setOrDelete(body['phases']['cold']['actions']['allocate'], !resource[:cold_allocate_include].nil?, 'include', resource[:cold_allocate_include])
-    setOrDelete(body['phases']['cold']['actions']['allocate'], !resource[:cold_allocate_exclude].nil?, 'exclude', resource[:cold_allocate_exclude])
+    setOrDelete(body['phases']['cold']['min_age'], !resource[:min_age].nil?, 'min_age', resource[:min_age])
     setOrDelete(body['phases']['cold']['actions']['allocate'], !resource[:cold_allocate_require].nil?, 'require', resource[:cold_allocate_require])
-    setOrDelete(body['phases']['cold']['actions'], resource[:cold_freeze], 'freeze', {})
-    setOrDelete(body['phases']['cold']['actions'], resource[:cold_unfollow], 'unfollow', {})
-    setOrDelete(body['phases']['delete']['actions'], resource[:delete_delete], 'delete', {})
     JSON.generate(body)
   end
 
--- lib/puppet/type/elasticsearch_hot_warm_cold_lifecycle_policy.rb
+++ lib/puppet/type/elasticsearch_hot_warm_cold_lifecycle_policy.rb
@@ -11,104 +11,12 @@ Puppet::Type.newtype(:elasticsearch_hot_warm_cold_lifecycle_policy) do
     desc 'Lifecycle policy name.'
   end
 
-  newproperty(:hot_priority) do
-    desc 'Priority set to indexes on hot phase'
-  end
-
-  newproperty(:hot_rollover_max_size) do
-    desc 'Max primary shard index storage size before doing rollover'
-  end
-
-  newproperty(:hot_rollover_max_docs) do
-    desc 'Max number of documents an index is to contain before rolling over'
-  end
-
-  newproperty(:hot_rollover_max_age) do
-    desc 'Max time elapsed from index creation before doing rollover'
-  end
-
-  newproperty(:hot_unfollow) do
-    desc 'This action turns a ccr follower index into a regular index'
-  end
-
-  newproperty(:warm_min_age) do
-    desc 'Indices enter phases based on a phase’s min_age parameter. The index will not enter the phase until the index’s age is older than that of the min_age'
-  end
-
-  newproperty(:warm_priority) do
-    desc 'Priority set to indexes on warm phase'
-  end
-
-  newproperty(:warm_unfollow) do
-    desc 'This action turns a ccr follower index into a regular index'
-  end
-
-  newproperty(:warm_allocate_number_of_replicas) do
-    desc 'The number of replicas to assign to the index'
-  end
-
-  newproperty(:warm_allocate_include) do
-    desc 'Assigns an index to nodes having at least one of the attributes'
-  end
-
-  newproperty(:warm_allocate_exclude) do
-    desc 'Assigns an index to nodes having none of the attributes'
-  end
-
-  newproperty(:warm_allocate_require) do
-    desc 'Assigns an index to nodes having all of the attributes'
-  end
-
-  newproperty(:warm_read_only) do
-    desc 'This action will set the index to be read-only'
-  end
-
-  newproperty(:warm_force_merge_max_num_segments) do
-    desc 'The number of segments to merge to. To fully merge the index, set it to 1'
-  end
-
-  newproperty(:warm_shrink_number_of_shards) do
-    desc 'The number of shards to shrink to. must be a factor of the number of shards in the source index'
-  end
-
   newproperty(:cold_min_age) do
     desc 'Indices enter phases based on a phase’s min_age parameter. The index will not enter the phase until the index’s age is older than that of the min_age.'
   end
 
-  newproperty(:cold_priority) do
-    desc 'Priority set to indexes on cold phase'
-  end
-
-  newproperty(:cold_allocate_number_of_replicas) do
-    desc 'The number of replicas to assign to the index'
-  end
-
-  newproperty(:cold_allocate_include) do
-    desc 'Assigns an index to nodes having at least one of the attributes'
-  end
-
-  newproperty(:cold_allocate_exclude) do
-    desc 'Assigns an index to nodes having none of the attributes'
-  end
-
   newproperty(:cold_allocate_require) do
     desc 'Assigns an index to nodes having all of the attributes'
-  end
-
-  newproperty(:cold_freeze) do
-    desc 'This action will freeze the index'
-  end
-
-  newproperty(:cold_unfollow) do
-    desc 'This action turns a ccr follower index into a regular index'
-  end
-
-  newproperty(:delete_min_age) do
-    desc 'Indices enter phases based on a phase’s min_age parameter. The index will not enter the phase until the index’s age is older than that of the min_age.'
-  end
-
-  newproperty(:delete_delete) do
-    desc 'The Delete Action does just that, it deletes the index'
   end
 
 end
